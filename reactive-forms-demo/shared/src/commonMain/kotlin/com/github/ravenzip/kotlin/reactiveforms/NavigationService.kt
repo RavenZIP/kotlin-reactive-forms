54d94bcd1a3c7defa4bbbbdd709189ed646f755f
RavenZIP <razor01478920@mail.ru>
2026-09-24 23:26:16 +0300

    Дибилизм

--- reactive-forms-demo/shared/src/commonMain/kotlin/com/github/ravenzip/kotlin/reactiveforms/NavigationService.kt
+++ reactive-forms-demo/shared/src/commonMain/kotlin/com/github/ravenzip/kotlin/reactiveforms/NavigationService.kt
@@ -1,13 +1,16 @@
 package com.github.ravenzip.kotlin.reactiveforms
 
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.ViewModel
 import com.github.ravenzip.kotlin.reactiveforms.data.Screen
 
 class RootNavigationViewModel : ViewModel() {
     val backStack = mutableStateListOf<Screen>(Screen.Home)
 
-    val componentScreens =
+    // Если не указать тип явно, то при использовании items(navigationViewModel.componentScreens) {
+    // item -> } тип определяется как Any и компиляция wasmJs\Js падает (android и jvm норм)
+    val componentScreens: SnapshotStateList<Screen> =
         mutableStateListOf(
             Screen.AutocompleteTextField,
             Screen.Switch,
