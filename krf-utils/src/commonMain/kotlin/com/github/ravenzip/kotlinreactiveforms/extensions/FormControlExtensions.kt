57f7e3fe331f3a81a09c38c6c7715d044dae28b4
RavenZIP <razor01478920@mail.ru>
2026-09-24 21:02:07 +0300

    Не актуально. Ошибка выводится на стороне компонента

--- krf-utils/src/commonMain/kotlin/com/github/ravenzip/kotlinreactiveforms/extensions/FormControlExtensions.kt
+++ krf-utils/src/commonMain/kotlin/com/github/ravenzip/kotlinreactiveforms/extensions/FormControlExtensions.kt
@@ -2,35 +2,10 @@ package com.github.ravenzip.kotlinreactiveforms.extensions
 
 import com.github.ravenzip.kotlinreactiveforms.data.FormControlSnapshot
 import com.github.ravenzip.kotlinreactiveforms.data.ValueWithTypeChange
-import com.github.ravenzip.kotlinreactiveforms.data.extractErrors
 import com.github.ravenzip.kotlinreactiveforms.form.FormControl
 import com.github.ravenzip.kotlinreactiveforms.validation.ValidationError
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-
-val <TValue, TError : ValidationError> FormControl<TValue, TError>.errorMessages: List<String>
-    get() = status.extractErrors().map { error -> error.message }
-
-val <TValue, TError : ValidationError> FormControl<TValue, TError>.firstErrorMessage: String
-    get() = status.extractErrors().firstOrNull()?.message ?: ""
-
-val <TValue, TError : ValidationError> FormControl<TValue, TError>.lastErrorMessage: String
-    get() = status.extractErrors().lastOrNull()?.message ?: ""
-
-fun <TValue, TError : ValidationError> FormControl<TValue, TError>.errorMessagesChanges():
-    Flow<List<String>> = stateChanges.map { state ->
-    state.status.extractErrors().map { error -> error.message }
-}
-
-fun <TValue, TError : ValidationError> FormControl<TValue, TError>.firstErrorMessageChanges():
-    Flow<String> = stateChanges.map { state ->
-    state.status.extractErrors().firstOrNull()?.message ?: ""
-}
-
-fun <TValue, TError : ValidationError> FormControl<TValue, TError>.lastErrorMessageChanges():
-    Flow<String> = stateChanges.map { state ->
-    state.status.extractErrors().lastOrNull()?.message ?: ""
-}
 
 val <T> FormControl<T, ValidationError>.currentValueWithTypeChange: ValueWithTypeChange<T>
     get() = ValueWithTypeChange(value, valueChangeType)
